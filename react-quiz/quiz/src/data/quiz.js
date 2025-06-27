7b8989dab631162a39d692deaf7dc13a21ba19ee
yukiyuki925 <yuki.work1999@gmail.com>
2025-06-27 11:45:02 +0900

    画面表示

---
+++ react-quiz/quiz/src/data/quiz.js
@@ -0,0 +1,29 @@
+const quizData = [
+  {
+    question: "Question1",
+    options: ["select1", "select2", "select3", "select4"],
+    answerIndex: 0,
+  },
+  {
+    question: "Question2",
+    options: ["select1", "select2", "select3", "select4"],
+    answerIndex: 1,
+  },
+  {
+    question: "Question3",
+    options: ["select1", "select2", "select3", "select4"],
+    answerIndex: 2,
+  },
+  {
+    question: "Question4",
+    options: ["select1", "select2", "select3", "select4"],
+    answerIndex: 3,
+  },
+  {
+    question: "Question5",
+    options: ["select1", "select2", "select3", "select4"],
+    answerIndex: 1,
+  },
+];
+
+export default quizData;
---
+++ react-quiz/quiz/src/pages/QuizPage.jsx
@@ -0,0 +1,22 @@
+import Button from "../components/Button/Button";
+import Display from "../components/Display/Display";
+import quizData from "../data/quiz";
+
+export default function QuizPage() {
+  const quizIndex = 0;
+  const handleClick = (clickedIndex) => {
+    console.log("clickedIndex:", clickedIndex);
+  };
+  return (
+    <>
+      <Display>{`Q1. ${quizData[quizIndex].question}`}</Display>
+      {quizData[quizIndex].options.map((option, index) => {
+        return (
+          <Button key={`option-${index}`} onClick={() => handleClick(index)}>
+            {option}
+          </Button>
+        );
+      })}
+    </>
+  );
+}
