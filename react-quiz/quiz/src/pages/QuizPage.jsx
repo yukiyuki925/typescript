484f06b86fee6a417b7e2e734bdf53b580ca853f
yukiyuki925 <yuki.work1999@gmail.com>
2025-06-28 22:01:11 +0900

    クイズアプリ完成

--- react-quiz/quiz/src/pages/QuizPage.jsx
+++ react-quiz/quiz/src/pages/QuizPage.jsx
@@ -1,22 +1,54 @@
+import { useEffect, useState } from "react";
 import Button from "../components/Button/Button";
 import Display from "../components/Display/Display";
 import quizData from "../data/quiz";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../const";
 
 export default function QuizPage() {
-  const quizIndex = 0;
+  const [quizIndex, setQuizIndex] = useState(0);
+  const [answerLogs, setAnswerLogs] = useState([]);
+  const navigation = useNavigate();
+  const MAX_QUIZ_LEN = quizData.length;
+
   const handleClick = (clickedIndex) => {
-    console.log("clickedIndex:", clickedIndex);
+    if (clickedIndex === quizData[quizIndex].answerIndex) {
+      setAnswerLogs((prev) => [...prev, true]);
+    } else {
+      setAnswerLogs((prev) => [...prev, false]);
+    }
+    setQuizIndex((prev) => prev + 1);
   };
+
+  useEffect(() => {
+    if (answerLogs.length === MAX_QUIZ_LEN) {
+      const correctNum = answerLogs.filter((answer) => {
+        return answer === true;
+      });
+      navigation(ROUTES.RESULT, {
+        state: {
+          maxQuizLen: MAX_QUIZ_LEN,
+          correctNumLen: correctNum.length,
+        },
+      });
+    }
+  }, [answerLogs, MAX_QUIZ_LEN, navigation]);
   return (
     <>
-      <Display>{`Q1. ${quizData[quizIndex].question}`}</Display>
-      {quizData[quizIndex].options.map((option, index) => {
-        return (
-          <Button key={`option-${index}`} onClick={() => handleClick(index)}>
-            {option}
-          </Button>
-        );
-      })}
+      {quizData[quizIndex] && (
+        <Display>{`Q${quizIndex + 1}. ${
+          quizData[quizIndex].question
+        }`}</Display>
+      )}
+      <br />
+      {quizData[quizIndex] &&
+        quizData[quizIndex].options.map((option, index) => {
+          return (
+            <Button key={`option-${index}`} onClick={() => handleClick(index)}>
+              {option}
+            </Button>
+          );
+        })}
     </>
   );
 }
